bee7ae8d6b30e24b8a874e67846aed968dd729ed
Matthieu Arene <matthieu.arene@chauffeur-prive.com>
2017-07-19 14:40:37 +0200

    modifs to parse

--- srcs/parse.c
+++ srcs/parse.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 #include "args.h"
 
-int			parse(t_parser* p, int argc, char** argv)
+static int8_t		is_long(char* str)
+{
+	return ((str && strlen(str) > 2 && strstr(str, "--") == str));
+}
+
+static int8_t		is_short(char* str)
+{
+	return ((str && strlen(str) > 1 && str[0] == '-' && str[1] != '-'));
+}
+
+static int8_t		is_arg(char* str)
+{
+	return (is_long(str) || is_short(str));
+}
+
+int							parse(t_parser* p, int argc, char** argv)
 {
 	t_token*	toks;
 	t_token*	it;
@@ -10,7 +26,7 @@ int			parse(t_parser* p, int argc, char** argv)
 	it = toks;
 	while (it)
 	{
-		printf("%s\n", it->value);
+		if ()
 		it = it->next;
 	}
 	return ARG_OK;
